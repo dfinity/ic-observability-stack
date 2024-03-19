d1685574b3464ead7927bb37b74bdd10e742e395
Manuel Amador <manuel.amador@dfinity.org>
2024-03-19 12:23:50 +0100

    Make bootstrap script more readable.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,61 +1,88 @@
 #!/bin/bash -e
 
-u=$(uname -s)
-case "$u" in
+UNSUPPORTED=32
+FAILED=16
+MANUAL_INTERVENTION=8
+
+check_ansible_version() {
+    if ansible --version | head -1 | grep -q '[[]core'
+    then
+        >&2 echo '* Ansible found and up-to-date enough.'
+    else
+        >&2 echo "Your system's Ansible version is too old"
+        >&2 ansible --version
+        >&@ echo "Please uninstall the existing Ansible and retry running this script.  See installation instructions provided by Ansible: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html "
+        exit $MANUAL_INTERVENTION
+    fi
+}
+
+got() {
+    which "$1" >/dev/null 2>&1
+}
+
+case "$(uname -s)" in
     Linux*) {
-        which pip3 >/dev/null || {
-            which apt-get && {
+        if got pip3
+        then
+            >&2 echo '* pip found and installed.'
+        else
+            if got apt-get
+            then
                 sudo apt-get install -y python3-pip || {
-                    >&2 echo pip installation failed.
-                    exit 32
+                    >&2 echo "* pip installation failed."
+                    exit $FAILED
                 }
-            } || which dnf && {
+            elif got dnf
+            then
                 sudo dnf install -y python3-pip || {
-                    >&2 echo pip installation failed.
-                    exit 32
+                    >&2 echo "* pip installation failed."
+                    exit $FAILED
                 }
-            } || {
-                >&2 echo "Your operating system is not currently supported by this script."
-                exit 32
-            }
-        }
-        which ansible >/dev/null && {
-            ansible --version | head -1 | grep -q '[[]core' || {
-                >&2 echo "Your system's Ansible version is too old"
-                >&2 ansible --version
-                >&@ echo "Please uninstall the existing Ansible and retry running this script.  See installation instructions provided by Ansible: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html "
-                exit 18
-            }
-        } || {
+            else
+                >&2 echo "Your operating system is not currently supported by this script.  Please install the Python pip package using your operating system's package manager or the documented method for your operating system."
+                exit $UNSUPPORTED
+            fi
+        fi
+        if got ansible
+        then
+            check_ansible_version
+        else
             pip3 install --user ansible || {
-                ret=$?
-                >&2 echo Ansible installation failed.
-                exit $ret
+                >&2 echo "* Ansible installation failed."
+                exit $FAILED
             }
-        }
+        fi
     };;
     Darwin*) {
-        which brew >/dev/null || {
+        if got brew
+        then
+            >&2 echo "* Homebrew found and installed."
+        else
             >&2 echo "Homebrew is not installed, but it is necessary.  Please install Homebrew by running:"
             >&2 echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
             >&2 read -p 'Do you want to run that command right now [y/n]?' install_brew
             if [ "$install_brew" == "y" ]
             then
-                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || exit $?
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || exit $FAILED
             else
-                exit 4
+                exit $MANUAL_INTERVENTION
             fi
-        }
-        which ansible >/dev/null || {
-            brew install ansible
-            exit 0
-        }
+        fi
+        if got ansible
+        then
+            check_ansible_version
+        else
+            brew install ansible || {
+                >&2 echo "* Ansible installation failed."
+                exit $FAILED
+            }
+        fi
     };;
     *) {
        >&2 echo "Your operating system is not currently supported by this script.  Please install the Ansible package using your operating system's package manager or the documented method for your operating system."
-        exit 32
+        exit $UNSUPPORTED
     };;
 esac
 
->&2 echo "Ansible is going to run on your machine, and you will now be prompted for your user account password (what Ansible calls 'BECOME password').  This will be used to become administrator and deploy various packages needed locally; if 'sudo' does not require a password on this machine, simply hit ENTER.  Follow onscreen instructions as the playbook runs.  If the next step just hangs for a long period of time, interrupt it and check that you typed your root password correctly."
+>&2 echo "Ansible is going to run on your machine to install some software, and you will now be prompted for your user account password (what Ansible calls 'BECOME password').  This will be used to become administrator and deploy various packages needed locally; if 'sudo' does not require a password on this machine, simply hit ENTER.  Follow onscreen instructions as the playbook runs.  If the next step hangs for a long period of time, interrupt it and check that you typed your root password correctly."
 ansible-playbook -v -K playbooks/prepare-local-system.yml
